ee652b59187db9ca612bd9d101fe120d6b3dcfdf
Evgeniy Kozlov <ek916569@gmail.com>
2024-04-16 21:21:17 +0500

    add profile page

--- frontend/app/(pages)/profile/page.tsx
+++ frontend/app/(pages)/profile/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { Avatar, Button, FormControl, FormLabel, Input, Flex, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+
 export default function ProfilePage() {
-    return <>Profile</>;
+	const [username, setUsername] = useState("GRIZLI");
+	const [password, setPassword] = useState("123456789");
+
+	const handleSave = () => {
+		// Здесь можно добавить логику для сохранения новых данных пользователя
+	};
+
+	return (
+		<div>
+			<Flex gap={6}>
+				<Avatar size="xl" name={"GRIZLI"} src="/avatar.jpg" />
+				<Flex direction={"column"} gap={1.5}>
+					<Heading>{"GRIZLI"}</Heading>
+					<Button size="sm" variant={"outline"}>
+						Сменить аватарку
+					</Button>
+				</Flex>
+			</Flex>
+			<FormControl mt={10}>
+				<FormLabel>Логин</FormLabel>
+				<Input value={username} onChange={e => setUsername(e.target.value)} />
+			</FormControl>
+			<FormControl mt={2}>
+				<FormLabel>Пароль</FormLabel>
+				<Input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+			</FormControl>
+			<Button mt={4} onClick={handleSave}>
+				Сохранить изменения
+			</Button>
+		</div>
+	);
 }
