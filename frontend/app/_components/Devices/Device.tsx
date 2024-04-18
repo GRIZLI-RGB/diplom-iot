d509a6e641411baad7f948d20f7810102d062669
GRIZLI-RGB <ek916569@gmail.com>
2024-04-18 19:32:54 +0500

    sync

--- frontend/app/_components/Devices/Device.tsx
+++ frontend/app/_components/Devices/Device.tsx
@@ -1,11 +1,25 @@
 "use client";
 
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import { Text, Box, Flex, IconButton, Switch } from "@chakra-ui/react";
+import { Text, Box, Flex, IconButton, Switch} from "@chakra-ui/react";
 import clsx from "clsx";
 import { BiBulb, BiCamera, BiTargetLock } from "react-icons/bi";
+import {useState} from "react";
+import {
+	Slider,
+	SliderTrack,
+	SliderFilledTrack,
+	SliderThumb,
+	SliderMark,
+} from '@chakra-ui/react'
 
 export default function Device({ device, onEditClick }: { device: Device; onEditClick: () => void }) {
+	const [brightness, setBrightness] = useState(50);
+
+	const handleBrightnessChange = (value: number) => {
+		setBrightness(value);
+	};
+
 	return (
 		<Box
 			borderWidth="1px"
@@ -25,6 +39,16 @@ export default function Device({ device, onEditClick }: { device: Device; onEdit
 				</Flex>
 				<Switch colorScheme="teal" size="md" />
 			</Flex>
+			{device.type === "light" && (<Box textAlign="center" p={4}>
+				<Slider aria-label='slider-ex-4' defaultValue={30}>
+					<SliderTrack bg='red.100'>
+						<SliderFilledTrack bg='tomato' />
+					</SliderTrack>
+					<SliderThumb boxSize={6}>
+						<Box color='tomato' as={MdGraphicEq} />
+					</SliderThumb>
+				</Slider>
+			</Box>)}
 			<Flex align="flex-end" mt={4} justify="space-between" gap={10}>
 				<Flex direction={"column"}>
 					<Text fontSize="13px" fontWeight={500} color={device.is_connected ? "green" : "red"}>
--- frontend/app/_components/Devices/DevicesFilters.tsx
+++ frontend/app/_components/Devices/DevicesFilters.tsx
@@ -6,16 +6,30 @@ export default function DevicesFilters() {
 	const handleGroup = by => {};
 
 	return (
-		<Box mt={10}>
+		<Flex mt={10} gap={5}>
 			<Flex align="center" justify="space-between" gap={2}>
 				<Text whiteSpace={"nowrap"} fontSize="sm" fontWeight={500}>
 					Группировать по
 				</Text>
 				<Select onChange={e => handleGroup(e.target.value)} size="sm" defaultValue={"is_connected"}>
-					<option value="type">Тип</option>
+					<option value="type">Все</option>
+					<option value="is_connected">Группа</option>
 					<option value="is_connected">Активность</option>
+					<option value="type">Тип</option>
 				</Select>
 			</Flex>
-		</Box>
+			<Flex align="center" justify="space-between" gap={2}>
+				<Text whiteSpace={"nowrap"} fontSize="sm" fontWeight={500}>
+					Сортировать по
+				</Text>
+				<Select onChange={e => handleGroup(e.target.value)} size="sm" defaultValue={"is_connected"}>
+					<option value="type">Все</option>
+					<option value="is_connected">Имя устройства (А {'->'} Я)</option>
+					<option value="is_connected">Имя устройства (Я {'->'} А)</option>
+					<option value="is_connected">Название группы (А {'->'} Я)</option>
+					<option value="is_connected">Название группы (Я {'->'} А)</option>
+				</Select>
+			</Flex>
+		</Flex>
 	);
 }
